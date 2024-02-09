f4034cc0679ba1e84498a6282492531cb4092440
arensin8 <arensinani4@gmail.com>
2024-02-09 13:12:47 +0330

    socket.io configs

--- client.js
+++ client.js
@@ -1,10 +1,22 @@
 const socket = io("http://localhost:3000");
+socket.on("broadCast", (data) => {
+  console.log(data);
+});
 
-socket.on('connect', data => {
-  socket.emit('welcome-client' ,' Hello serevr , I am client' )
-  socket.on('welcome-server' , data => {
+
+const teachersSocket = io("http://localhost:3000/teachers");
+teachersSocket.on("connect", (data) => {
+  teachersSocket.emit("teachersClient", " message from teachers namespace");
+  teachersSocket.on("techersServer", (data) => {
     console.log(data);
-  } )
-})
+  });
+});
 
+const studentsSocket = io("http://localhost:3000/students");
 
+studentsSocket.on("connect", (data) => {
+  studentsSocket.emit("studentsClient", "message from students namespace");
+  studentsSocket.on("studentsServer", (data) => {
+    console.log(data);
+  });
+});
--- server.js
+++ server.js
@@ -1,17 +1,32 @@
 const http = require("http");
 const WebSocket = require("ws");
-const server = http.createServer();
+const express = require("express");
+const app = express();
+const server = http.createServer(app);
+app.use(express.static("./"));
 // const ws = new WebSocket.Server({ server });
 const socketIO = require("socket.io");
+const exp = require("constants");
 const io = socketIO(server, {
   cors: {
     origin: "*",
   },
+  serverClient: true,
 });
 
-io.on("connection", (socket) => {
-  socket.emit("welcome-server", "Hello client , I am server");
-  socket.on("welcome-client", (data) => {
+io.on("connection", () => {
+  io.emit("broadCast","hello everyone");
+});
+io.of("/teachers").on("connection", (socket) => {
+  socket.emit("techersServer", "Hello teachers , I am server");
+  socket.on("teachersClient", (data) => {
+    console.log(data);
+  });
+});
+
+io.of("/students").on("connection", (socket) => {
+  socket.emit("studentsServer", "Hello students , I am server");
+  socket.on("studentsClient", (data) => {
     console.log(data);
   });
 });
